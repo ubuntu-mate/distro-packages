e7c178dd3db28ae49345a8643ee8d67caa1d740e
Simon Quigley <tsimonq2@ubuntu.com>
2017-09-03 02:50:13 -0500

    Delete unneeded files.

--- debian/ubuntu-mate-guide/debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(ubuntu-mate-guide):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="ubuntu-mate-guide" command="/usr/bin/ubuntu-mate-guide"
--- debian/ubuntu-mate-guide/debian/ubuntu-mate-guide.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the ubuntu-mate-guide package
-#
-0 4	* * *	root	[ -x /usr/bin/ubuntu-mate-guide_maintenance ] && /usr/bin/ubuntu-mate-guide_maintenance
